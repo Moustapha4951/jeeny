c006b06ca3fab95dc981c31c27f6c7cbd7c1f7ea
Moustapha <moustapha@jeeny.com>
2026-04-18 14:19:40 +0000

    fix: remove rides controller import

--- src/modules/rides/rides.module.ts
+++ src/modules/rides/rides.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 // import { RidesService } from './rides.service';
 import { FareService } from './fare.service';
 import { MatchingService } from './matching.service';
-import { RidesController } from './rides.controller';
+// import { RidesController } from './rides.controller';
 
 @Module({
   controllers: [], // Temporarily disabled: RidesController
